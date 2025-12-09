369b3c48b93516c707116573fa256aac4aa6d56f
PiotrGrab <piotr.tomasz.grabowski@gmail.com>
2025-12-09 13:35:30 +0100

    fixing krippendorff_.py

--- code/krippendorff_.py
+++ code/krippendorff_.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import krippendorff
 
-df = pd.read_csv('Annotation_results.csv')
+df = pd.read_csv('../data/Annotation_results.csv')
 arr = df[['Piotr', 'Ondrej', 'Jakub', 'Hanka']].to_numpy().T
 alpha = krippendorff.alpha(reliability_data=arr, level_of_measurement='ordinal')
 print(alpha)
